14b5ede34f65e46920167f7ad5ed32981d4b21bf
Michael <mueller@s-p-it.de>
2019-06-25 20:20:59 +0200

    add rainDuration check

--- src/service/DatabaseService.py
+++ src/service/DatabaseService.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
 from datetime import date, datetime, timedelta
+from __builtin__ import False
 
 class DatabaseService:
     
@@ -10,8 +11,8 @@ class DatabaseService:
     def _createTables(self):
         self.connection = self.getConnection()
         cursor = self.connection.cursor()
-        cursor.execute("CREATE TABLE IF NOT EXISTS temperature (temperature real,messTime int)")
-        cursor.execute("CREATE TABLE IF NOT EXISTS rain(rain int,rainTime int)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS temperature (temperature real, messTime int)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS rain(rain int, rainTime int)")
         cursor.execute("CREATE TABLE IF NOT EXISTS powerLog (status int, time int)")
         self.connection.commit()
             
@@ -26,15 +27,17 @@ class DatabaseService:
         cursor.execute("INSERT INTO temperature (temperature, messTime) VALUES (?, ?)", (int(temperature), date.today()))
         self.connection.commit()
 
+    # save the given rain status for the actual messuring
     def saveRainStatus(self, rain):
         cursor = self.connection.cursor()
         statement = "INSERT INTO rain (rain, rainTime) VALUES (?, ?)"
         cursor.execute(statement, (rain, date.today()))
         self.connection.commit()
 
+    # save the date when it is switched on and off
     def saveSwitchedOn(self, status):
         cursor = self.connection.cursor()
-        statement = "INSERT INTO powerLOg (status, time) VALUES (?, ?)"
+        statement = "INSERT INTO powerLog (status, time) VALUES (?, ?)"
         cursor.execute(statement, (status, date.today()))
         self.connection.commit()        
         
@@ -57,7 +60,6 @@ class DatabaseService:
         cursor.execute('''INSERT INTO rain (rain, rainTime) VALUES (?,?)''', (1, '2019-06-23 15:23:34'))
         cursor.execute('''INSERT INTO rain (rain, rainTime) VALUES (?,?)''', (0, '2019-06-22 15:23:34'))
         checkDate = checkDate.strftime('%s')
-        # change to rain table
         cursor.execute("""SELECT rain, date(rainTime) FROM rain WHERE rainTime > ?""", ([checkDate]))
         result = cursor.fetchall()
         checkYesterday = (date.today() - timedelta(days=1)).strftime('%Y-%m-%d')
@@ -66,13 +68,26 @@ class DatabaseService:
         for row in result:
             if (bool(row[0])):
                 if (row[1] == checkYesterday):
-                    # add check how long it has rained
-                    checkResult = True
+                    if (self.checkRainDuration(row[1]) > 15):
+                        checkResult = True
+                    else:
+                        checkResult = False
                 elif(row[1] == checkBeforeYesterday):
                     checkResult &= False
                 else:
                     checkResult &= False
         return checkResult
+
+    def checkRainDuration(self, toCheck):
+        cursor = self.connection.cursor()
+        cursor.execute('''SELECT rain FROM rain WHERE rainTime = ?''', toCheck)
+        result = cursor.fetchall()
+        duration = 0;
+        for row in result:
+            if (row[0] == 1):
+                duration += 3
+                
+        return duration
 
     def closeConnection(self):
         self.connection.close
