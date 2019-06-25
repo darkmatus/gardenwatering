4fd5d3e4be638d7770ef94a4eaf891514615e206
Michael <mueller@s-p-it.de>
2019-06-25 19:31:54 +0200

    merge more files

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# gardenwatering
+Python project for a garden watering system based on a raspberry pi (3B+), DHT22, HL-83 and a EnerGenie Programmable power strip (EG-PM2)
+
 Installing needed Software:
 
 ```
--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 #from src.Timer import Timer
 #from src.UsbSwitcher import UsbSwitcher
-from src.TempChecker import TempChecker
-#from src.RainChecker import RainChecker
+#from src.TempChecker import TempChecker
+from src.RainChecker import RainChecker
 import sched, time
 
 #rainScheduler = sched.scheduler(time.time, time.sleep)
 tempScheduler = sched.scheduler(time.time, time.sleep)
 
 #def rainChecker(sc):
-#    rainCheck = RainChecker()
-#    rainCheck.check
+rainCheck = RainChecker()
+print "Call Rain Check..."
+rainCheck.checkRain()
 #    rainScheduler.enter(600, 1, rainChecker, (sc,))
 
 #rainScheduler.enter(10, 1, rainChecker, (rainScheduler,))
 #rainScheduler.run()
 
-def tempCheck(tempSched):
-    tempChecker = TempChecker()
-    tempChecker.check()
-    tempScheduler.enter(14400, 1, tempCheck, (tempSched,))
+#def tempCheck(tempSched):
+#    tempChecker = TempChecker()
+#    tempChecker.check()
+#    tempScheduler.enter(14400, 1, tempCheck, (tempSched,))
 
-tempScheduler.enter(11, 1, tempCheck, (tempScheduler,))
-tempScheduler.run()
+#tempScheduler.enter(11, 1, tempCheck, (tempScheduler,))
+#tempScheduler.run()
 #switcher = UsbSwitcher()
 #switcher.switch()
 #timer = Timer()
